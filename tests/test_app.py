77968617af8a67f0c608e9303e5fd431b88ece4e
shri-hari27 <shrihari2027@gmail.com>
2026-07-23 17:20:19 +0000

    Add automated tests to CI pipeline

---
+++ tests/test_app.py
@@ -0,0 +1,14 @@
+import os
+import sys
+
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
+
+from app.app import app
+
+
+def test_home():
+    client = app.test_client()
+
+    response = client.get("/")
+
+    assert response.status_code == 200
